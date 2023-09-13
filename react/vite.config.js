b598f95d60555a5b20ee14539b1bfc29820a6f08
siradjedd <siradjeddinefisli@gmail.com>
2023-09-13 02:29:58 +0100

    docker-compose configuration

--- react/vite.config.js
+++ react/vite.config.js
@@ -6,6 +6,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    //port: 5173, When not running with docker compose, this is the port which will be used in docker
-  }
+    watch: {
+      // Exclude the 'node_modules' directory from file watching
+      exclude: ['node_modules/**'],
+    },
+    // Optionally, you can specify a custom port
+    // port: 5173, When not running with docker compose, this is the port which will be used in docker
+  },
 })
